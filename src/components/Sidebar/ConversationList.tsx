90aec2f06d3eb6dac6cb981f8c0ff6f2b554b993
jabjubinski <jabajnldz@gmail.com>
2025-10-24 15:55:49 +0400

    remove reverse

--- src/components/Sidebar/ConversationList.tsx
+++ src/components/Sidebar/ConversationList.tsx
@@ -46,7 +46,7 @@ export default function ConversationList() {
               conversation={conversation}
             />
           </>
-        )).reverse()}
+        ))}
       </div>
     </div>
   );
