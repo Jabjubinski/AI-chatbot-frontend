a23f304d8b1ca208f44043c754d92b912f2b512a
jabjubinski <jabajnldz@gmail.com>
2025-11-10 18:35:10 +0400

    redesigned conversationStartPage.

--- src/pages/ConversationStartPage.tsx
+++ src/pages/ConversationStartPage.tsx
@@ -8,6 +8,7 @@ import CustomButton from "../components/UI/CustomButton";
 import icons from "../components/UI/icons";
 import { useNavigate } from "react-router-dom";
 import LoadingCircleSpinner from "../components/UI/LoadingCircle";
+import { motion } from "motion/react";
 
 function ConversationCreate() {
   const [selectedTags, setSelectedTags] = useState<number[]>([]);
@@ -22,7 +23,7 @@ function ConversationCreate() {
   const navigate = useNavigate();
 
   console.log(selectedTags);
-  console.log(assistants);
+  console.log(selectedAssistants);
 
   useEffect(() => {
     fetchAssistantTags();
@@ -30,6 +31,7 @@ function ConversationCreate() {
 
   useEffect(() => {
     fetchAssistants(selectedTags);
+    console.log("heyyyy", assistants);
   }, [selectedTags, fetchAssistants]);
 
   const handleSubmit = async () => {
@@ -53,10 +55,60 @@ function ConversationCreate() {
   return (
     <div className="h-screen w-full bg-[#121826] text-slate-100 overflow-hidden">
       {/* Content */}
-      <div className="relative z-10 h-screen">
-        <div className="flex flex-col h-4/8 border-r border-slate-800/60 overflow-hidden">
-          {/* Header */}
-          <div className="h-2/8 px-8 py-6 border-b border-slate-800/60 bg-slate-900/30 backdrop-blur-sm">
+      <div className="relative z-10 h-full flex flex-col border-r border-slate-800/60 overflow-hidden">
+        {/* Header */}
+
+        {/* Category Tags */}
+        {/* <div className="px-8 h-1/8 flex flex-wrap  py-4 border-b border-slate-800/60">
+          <div className="flex flex-wrap gap-2">
+            <CustomButton
+              disabled={selectedTags.length === 0}
+              onClick={() => setSelectedTags([])}
+              className={clsx(
+                "px-4 py-2 relative rounded-lg text-sm font-medium transition-all duration-200",
+                selectedTags.length === 0
+                  ? "bg-gradient-to-r bg-[#2c3a4a] border-[#0a0e14] text-white shadow-lg shadow-black/30"
+                  : "bg-slate-800/50 text-slate-400 border border-slate-700/50 hover:bg-slate-800/80 hover:text-slate-300 hover:border-slate-600"
+              )}
+            >
+              {selectedAssistants.length > 0 && (
+                <span className="px-2 py-0.5 text-black left-[85%] z-50 bottom-[70%] absolute rounded-full bg-white">
+                  {selectedAssistants.length}
+                </span>
+              )}
+              All
+            </CustomButton>
+            {assistant_tags.map((tag) => (
+              <CustomButton
+                key={tag.id}
+                onClick={() =>
+                  selectedTags.includes(tag.id)
+                    ? setSelectedTags((prev) =>
+                        prev.filter((f) => f !== tag.id)
+                      )
+                    : setSelectedTags((prev) => [...prev, tag.id])
+                }
+                className={clsx(
+                  "px-4 py-2 relative rounded-lg text-sm font-medium transition-all duration-200",
+                  selectedTags.includes(tag.id)
+                    ? "bg-gradient-to-r bg-[#2c3a4a] border-[#0a0e14] text-white shadow-lg shadow-black/30"
+                    : "bg-slate-800/50 text-slate-400 border border-slate-700/50 hover:bg-slate-800/80 hover:text-slate-300 hover:border-slate-600"
+                )}
+              >
+                {selectedAssistants.filter((a) => a.id === tag.id).length >
+                  0 && (
+                  <span className="px-2 py-0.5 left-[85%] z-50 bg-slate-800/70 bottom-[70%] absolute rounded-full text-white">
+                    {selectedAssistants.filter((a) => a.id === tag.id).length}
+                  </span>
+                )}
+                {tag.name}
+              </CustomButton>
+            ))}
+          </div>
+        </div> */}
+
+        <div className="flex text-center h-6/8 flex-col justify-center items-center gap-3">
+          <div className="px-8 py-6">
             <h1 className="text-3xl font-bold bg-gradient-to-r text-white opacity-80 bg-clip-text mb-2">
               Start a Conversation
             </h1>
@@ -65,30 +117,23 @@ function ConversationCreate() {
             </p>
           </div>
 
-          {/* Search */}
-          <div className="px-8 h-2/8 py-4 border-b border-slate-800/60 bg-slate-900/20 backdrop-blur-sm">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search assistants..."
-                className="w-full px-4 py-3 rounded-lg bg-slate-800/50 border border-slate-700/50 text-slate-100 placeholder:text-slate-500 transition-all duration-200 focus:border-blue-500/50 focus:ring-2 focus:ring-blue-500/20 outline-none"
-              />
-            </div>
-          </div>
-
-          {/* Category Tags */}
-          <div className="px-8 py-4 border-b border-slate-800/60 bg-slate-900/20 backdrop-blur-sm">
+          <div className="px-8 w-1/2 flex flex-wrap   ">
             <div className="flex flex-wrap gap-2">
               <CustomButton
                 disabled={selectedTags.length === 0}
                 onClick={() => setSelectedTags([])}
                 className={clsx(
-                  "px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200",
+                  "px-4 py-2 relative rounded-lg text-sm font-medium transition-all duration-200",
                   selectedTags.length === 0
                     ? "bg-gradient-to-r bg-[#2c3a4a] border-[#0a0e14] text-white shadow-lg shadow-black/30"
                     : "bg-slate-800/50 text-slate-400 border border-slate-700/50 hover:bg-slate-800/80 hover:text-slate-300 hover:border-slate-600"
                 )}
               >
+                {selectedAssistants.length > 0 && (
+                  <span className="px-2 py-0.5 text-black left-[85%] z-50 bottom-[70%] absolute rounded-full bg-white">
+                    {selectedAssistants.length}
+                  </span>
+                )}
                 All
               </CustomButton>
               {assistant_tags.map((tag) => (
@@ -102,88 +147,106 @@ function ConversationCreate() {
                       : setSelectedTags((prev) => [...prev, tag.id])
                   }
                   className={clsx(
-                    "px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200",
+                    "px-4 py-2 relative rounded-lg text-sm font-medium transition-all duration-200",
                     selectedTags.includes(tag.id)
                       ? "bg-gradient-to-r bg-[#2c3a4a] border-[#0a0e14] text-white shadow-lg shadow-black/30"
                       : "bg-slate-800/50 text-slate-400 border border-slate-700/50 hover:bg-slate-800/80 hover:text-slate-300 hover:border-slate-600"
                   )}
                 >
+                  {selectedAssistants.filter((a) => a.id === tag.id).length >
+                    0 && (
+                    <span className="px-2 py-0.5 left-[85%] z-50 bg-slate-800/70 bottom-[70%] absolute rounded-full text-white">
+                      {selectedAssistants.filter((a) => a.id === tag.id).length}
+                    </span>
+                  )}
                   {tag.name}
                 </CustomButton>
               ))}
             </div>
           </div>
 
-          {loading ? ( 
-            <div className="flex w-full py-20 justify-center">
-              <LoadingCircleSpinner />
-            </div>
-          ) : (
-            <div className="px-8 py-6">
-              <div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 overflow-y-scroll gap-4">
-                {/* Selected Assistant Card */}
+          <div className="w-full flex flex-col items-center gap-4 justify-center ">
+            {/* Categories */}
+            <div className=" w-1/2 overflow-y-auto">
+              {loading ? (
+                <div className="flex w-full py-20 justify-center">
+                  <LoadingCircleSpinner />
+                </div>
+              ) : (
+                <div className="px-8 py-6">
+                  <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 overflow-y-scroll gap-4">
+                    {/* Selected Assistant Card */}
 
-                {assistants.map((assistant) => (
-                  <CustomButton
-                    onClick={() =>
-                      selectedAssistants.some(
-                        (_assistant) => _assistant.id === assistant.id
-                      )
-                        ? setSelectedAssistants((prev) =>
-                            prev.filter((f) => f.id !== assistant.id)
+                    {assistants.map((assistant) => (
+                      <CustomButton
+                        onClick={() =>
+                          selectedAssistants.some(
+                            (_assistant) => _assistant.id === assistant.id
                           )
-                        : setSelectedAssistants((prev) => [...prev, assistant])
-                    }
-                    className={clsx(
-                      "relative group p-4 h-32 rounded-xl border-2 transition-all duration-300 text-left shadow-xl",
-                      selectedAssistants.some(
-                        (_assistant) => _assistant.id === assistant.id
-                      )
-                        ? "border-[#0a0e14]/60 bg-slate-800/80"
-                        : "border-slate-700/50  bg-slate-800/40"
-                    )}
-                  >
-                    <div
-                      className={clsx(
-                        "absolute top-3 right-3 w-5 h-5 rounded-md border-2 flex items-center justify-center transition-all duration-200 bg-gradient-to-r",
-                        selectedAssistants.some(
-                          (_assistant) => _assistant.id === assistant.id
-                        )
-                          ? "bg-emerald-400 border-slate-800"
-                          : "border-slate-700 group-hover:border-slate-500"
-                      )}
-                    >
-                      {selectedAssistants.some(
-                        (_assistant) => _assistant.id === assistant.id
-                      ) && (
-                        <img
-                          src={icons.check.src}
-                          alt={icons.check.alt}
-                          className="w-3 h-3"
-                        />
-                      )}
-                    </div>
-                    <div className="pr-8">
-                      <h3 className="font-semibold text-slate-100 mb-1">
-                        {assistant.name}
-                      </h3>
-                      <p className="text-sm text-slate-400 leading-tight mb-2 line-clamp-1 sm:line-clamp-0">
-                        {assistant.description}
-                      </p>
-                      <span className="inline-block px-2 py-1 text-xs rounded-md bg-slate-700/50 text-slate-300 border border-slate-600/50">
-                        {assistant.tag}
-                      </span>
-                    </div>
-                  </CustomButton>
-                ))}
-              </div>
+                            ? setSelectedAssistants((prev) =>
+                                prev.filter((f) => f.id !== assistant.id)
+                              )
+                            : setSelectedAssistants((prev) => [
+                                ...prev,
+                                assistant,
+                              ])
+                        }
+                        className={clsx(
+                          "relative group p-4 h-32 rounded-xl border-2 transition-all duration-300 text-left shadow-xl",
+                          selectedAssistants.some(
+                            (_assistant) => _assistant.id === assistant.id
+                          )
+                            ? "border-[#0a0e14]/60 bg-slate-800/80"
+                            : "border-slate-700/50  bg-slate-800/40"
+                        )}
+                      >
+                        <div
+                          className={clsx(
+                            "absolute top-3 right-3 w-5 h-5 rounded-md border-2 flex items-center justify-center transition-all duration-200 bg-gradient-to-r",
+                            selectedAssistants.some(
+                              (_assistant) => _assistant.id === assistant.id
+                            )
+                              ? "bg-emerald-400 border-slate-800"
+                              : "border-slate-700 group-hover:border-slate-500"
+                          )}
+                        >
+                          {selectedAssistants.some(
+                            (_assistant) => _assistant.id === assistant.id
+                          ) && (
+                            <img
+                              src={icons.check.src}
+                              alt={icons.check.alt}
+                              className="w-3 h-3"
+                            />
+                          )}
+                        </div>
+                        <div className="pr-8">
+                          <h3 className="font-semibold text-xl text-slate-100 mb-1">
+                            {assistant.name}
+                          </h3>
+                          <p className="text-sm text-slate-400 leading-tight mb-2 line-clamp-1 sm:line-clamp-0">
+                            {assistant.description}
+                          </p>
+                          <span className="inline-block px-2 py-1 text-xs rounded-md bg-slate-700/50 text-slate-300 border border-slate-600/50">
+                            {assistant.tag}
+                          </span>
+                        </div>
+                      </CustomButton>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
 
-        {/* Assistants Grid */}
-
-        <div className="w-full h-4/8 justify-end backdrop-blur-sm flex flex-col">
+        <motion.div
+          animate={{
+            y: selectedAssistants.length > 0 ? 0 : "100%",
+            opacity: selectedAssistants.length > 0 ? 1 : 0,
+          }}
+          className="w-full h-2/8 justify-end backdrop-blur-sm flex flex-col"
+        >
           {/* Message Input */}
           <div className="px-6 py-4 flex w-full justify-center">
             <div className="w-1/2 h-14 flex relative">
@@ -192,9 +255,15 @@ function ConversationCreate() {
                 onChange={(e) => setPrompt(e.target.value)}
                 value={prompt}
                 disabled={selectedAssistants.length < 1 || loading}
-                placeholder={selectedAssistants.length === 0 ? "Please select at least one assistant..." : "Share your thoughts..."}
-                className={clsx("w-full px-4 py-3 rounded-lg border  border-slate-700/50 outline-none transition-all duration-200 resize-none bg-slate-800/50 text-slate-100 placeholder:text-slate-500 focus:border-[#0a0e14]/50 focus:ring-2 focus:ring-blue-500/20 hover:border-slate-600",
-                (selectedAssistants.length === 0 || loading) && "cursor-not-allowed bg-slate-800"
+                placeholder={
+                  selectedAssistants.length === 0
+                    ? "Please select at least one assistant..."
+                    : "Share your thoughts..."
+                }
+                className={clsx(
+                  "w-full px-4 py-3 rounded-lg border  border-slate-700/50 outline-none transition-all duration-200 resize-none bg-slate-800/50 text-slate-100 placeholder:text-slate-500 focus:border-[#0a0e14]/50 focus:ring-2 focus:ring-blue-500/20 hover:border-slate-600",
+                  (selectedAssistants.length === 0 || loading) &&
+                    "cursor-not-allowed bg-slate-800"
                 )}
               />
 
@@ -202,8 +271,10 @@ function ConversationCreate() {
                 <CustomButton
                   onClick={handleSubmit}
                   disabled={selectedAssistants.length < 1 || loading}
-                  className={clsx("p-3 rounded-full absolute right-2 top-2 font-semibold transition-all duration-300 flex items-center justify-center gap-2 bg-gradient-to-r bg-[#2c3a4a] text-white hover:shadow-lg hover:shadow-gray-900 hover:-translate-y-0.5 active:translate-y-0",
-                    (selectedAssistants.length < 1 || loading) && "cursor-not-allowed hover:bg-[#2c3a4a] hover:text-white hover:shadow-none hover:translate-y-0",
+                  className={clsx(
+                    "p-3 rounded-full absolute right-2 top-2 font-semibold transition-all duration-300 flex items-center justify-center gap-2 bg-gradient-to-r bg-[#2c3a4a] text-white hover:shadow-lg hover:shadow-gray-900 hover:-translate-y-0.5 active:translate-y-0",
+                    (selectedAssistants.length < 1 || loading) &&
+                      "cursor-not-allowed hover:bg-[#2c3a4a] hover:text-white hover:shadow-none hover:translate-y-0"
                   )}
                 >
                   <img
@@ -215,7 +286,7 @@ function ConversationCreate() {
               </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
